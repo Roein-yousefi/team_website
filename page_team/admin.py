42927ca859b7637c77b686ab50cbda8889da452e
Roein-yousefi <roeinyousefi@gmail.com>
2024-07-12 05:24:57 -0700

    create 3 models for website

--- page_team/admin.py
+++ page_team/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import TeamNews
+from .models import TeamNews , TeamShop , TeamGallery
 
 
 class TeamNewsAdmin(admin.ModelAdmin):
     list_display = ('title' , 'datetime_created')
 
+class TeamShopAdmin(admin.ModelAdmin):
+    list_display = ('title' , 'datetime_created' , 'price')
+
 
 admin.site.register(TeamNews , TeamNewsAdmin)
+admin.site.register(TeamShop , TeamShopAdmin)
+admin.site.register(TeamGallery , TeamNewsAdmin)
--- page_team/models.py
+++ page_team/models.py
@@ -5,8 +5,36 @@ from django.contrib.auth import get_user_model
 class TeamNews(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='news/' , blank=True , null=True)
+    image = models.ImageField(upload_to='newas/' , blank=True , null=True)
     datetime_created = models.DateTimeField(auto_now_add=True , null=True , blank=True)
 
     def __str__(self):
         return self.title
+
+class TeamShop(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='newas/' , blank=True , null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True , null=True , blank=True)
+    price = models.IntegerField()
+    def __str__(self):
+        return self.title
+
+
+class TeamGallery(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='newas/' , blank=True , null=True)
+    datetime_created = models.DateTimeField(auto_now_add=True , null=True , blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TeamPlayer(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='newas/' , blank=True , null=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
