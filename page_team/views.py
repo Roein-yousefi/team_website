db92163b535f118aa73ffa43cba82c53331814dd
Roein-yousefi <roeinyousefi@gmail.com>
2024-07-13 05:43:02 -0700

    edited home.html for las news

--- page_team/views.py
+++ page_team/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import get_object_or_404 , render
 
 from .models import TeamNews , TeamGallery , TeamPlayer , TeamShop
 
-class HomePageView(generic.ListView):
-    model = TeamNews
+class HomePageView(generic.TemplateView):
     template_name = 'page_team/home.html'
-    context_object_name = 'items'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        model1 = TeamNews.objects.order_by('-datetime_created')[:3]
+
+
+        context['model1'] = model1
+
+        return context
 
 class ShopPageView(generic.ListView):
     model = TeamShop
